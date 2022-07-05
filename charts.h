74986073246391c9c55107a5366399580aa3ec2c
Sergey <sergey.ryzhov2001@yandex.ru>
2022-07-05 08:25:55 +0300

    Перенёс все граф. функции в хеддер charts

--- charts.h
+++ charts.h
@@ -4,7 +4,8 @@
 #include "functions.h"
 #include "ui_widget.h"
 
-void plot_deriv_for_point(const QVector<Point_curve>& data_NURBS, Ui::Widget* ui)
+// Рисует линию производной в точке (касательную)
+void derivative_point_line(const QVector<Point_curve>& data_NURBS, Ui::Widget* ui)
 {
     for(const auto& point: data_NURBS)
     {
@@ -15,6 +16,140 @@ void plot_deriv_for_point(const QVector<Point_curve>& data_NURBS, Ui::Widget* ui
     }
 
     ui->graph_function->replot();
+}
+
+// Рисует кривую и многоугольник по заданным точкам
+void curve_plot(const QVector<QVector<double>>& b, const QVector<Point_curve>& data_NURBS, const int& x_min, const int& x_max, const int& y_min, const int& y_max,
+                const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
+{
+    ui->graph_function->clearGraphs(); // Очищаем все графики
+
+    QCPCurve *curve_point = new QCPCurve(ui->graph_function->xAxis, ui->graph_function->yAxis);
+
+    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
+    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
+    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
+
+    QPen pen;
+    pen.setWidth(2); // Устанавливаем ширину
+    curve_point->setPen(pen);
+
+    for(const auto& el: b) // Рисуем точки
+        curve_point->addData(el[0], el[1]);
+
+    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
+
+    QCPCurve *curve_spline = new QCPCurve(ui->graph_function->xAxis, ui->graph_function->yAxis);
+    pen.setColor(QColor(30, 144, 255));
+    curve_spline->setPen(pen);
+
+    for(const auto& el: data_NURBS) // Рисуем сплайн
+        curve_spline->addData(el.curve.first, el.curve.second);
+
+    ui->graph_function->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
+
+    ui->graph_function->legend->setVisible(true); // Включаем легенду графика
+    curve_point->setName(labels_legend_1);
+    curve_spline->setName(labels_legend_2);
+
+    // Подписываем оси Ox и Oy
+    ui->graph_function->xAxis->setLabel("Ось X");
+    ui->graph_function->yAxis->setLabel("Ось Y");
+
+    // Установим область, которая будет показываться на графике
+    ui->graph_function->xAxis->setRange(x_min, x_max); // Для оси Ox
+    ui->graph_function->yAxis->setRange(y_min, y_max); // Для оси Oy
+
+    ui->graph_function->plotLayout()->insertRow(0); // Вставляем строку
+    ui->graph_function->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_function, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
+
+    ui->graph_function->replot();
+}
+
+// Рисует первую производную и многоугольник по заданным точкам
+void first_derivative_plot(const QVector<QVector<double>>& data_point, const QVector<Point_curve>& data_NURBS, const int& x_min, const int& x_max, const int& y_min, const int& y_max,
+                           const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
+{
+    ui->graph_first_derivative->clearGraphs(); // Очищаем все графики
+
+    QCPCurve *curve_point = new QCPCurve(ui->graph_first_derivative->xAxis, ui->graph_first_derivative->yAxis);
+
+    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
+    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
+    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
+
+    for(const auto& el: data_point) // Рисуем точки
+        curve_point->addData(el[0], el[1]);
+
+    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
+
+    QCPCurve *curve_deriv { nullptr };
+    curve_deriv = new QCPCurve(ui->graph_first_derivative->xAxis, ui->graph_first_derivative->yAxis);
+
+    for(const auto& el: data_NURBS) // Рисуем сплайн
+        curve_deriv->addData(el.derivative_1.first, el.derivative_1.second);
+
+    ui->graph_first_derivative->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
+
+    ui->graph_first_derivative->legend->setVisible(true); // Включаем Легенду графика
+    curve_point->setName(labels_legend_1);
+    curve_deriv->setName(labels_legend_2);
+
+    // Подписываем оси Ox и Oy
+    ui->graph_first_derivative->xAxis->setLabel("Ось X");
+    ui->graph_first_derivative->yAxis->setLabel("Ось Y");
+
+    // Установим область, которая будет показываться на графике
+    ui->graph_first_derivative->xAxis->setRange(x_min, x_max); // Для оси Ox
+    ui->graph_first_derivative->yAxis->setRange(y_min, y_max); // Для оси Oy
+
+    ui->graph_first_derivative->plotLayout()->insertRow(0); // Вставляем строку
+    ui->graph_first_derivative->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_first_derivative, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
+
+    ui->graph_first_derivative->replot();
+}
+
+// Рисует вторую производную и многоугольник по заданным точкам
+void second_derivative_plot(const QVector<QVector<double>>& data_point, const QVector<Point_curve>& data_NURBS, const int& x_min, const int& x_max, const int& y_min, const int& y_max,
+                            const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
+{
+    ui->graph_second_derivative->clearGraphs(); // Очищаем все графики
+
+    QCPCurve *curve_point = new QCPCurve(ui->graph_second_derivative->xAxis, ui->graph_second_derivative->yAxis);
+
+    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
+    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
+    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
+
+    for(const auto& el: data_point) // Рисуем точки
+        curve_point->addData(el[0], el[1]);
+
+    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
+
+    QCPCurve *curve_deriv { nullptr };
+    curve_deriv = new QCPCurve(ui->graph_second_derivative->xAxis, ui->graph_second_derivative->yAxis);
+
+    for(const auto& el: data_NURBS) // Рисуем сплайн
+        curve_deriv->addData(el.derivative_2.first, el.derivative_2.second);
+
+    ui->graph_second_derivative->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
+
+    ui->graph_second_derivative->legend->setVisible(true); // Включаем Легенду графика
+    curve_point->setName(labels_legend_1);
+    curve_deriv->setName(labels_legend_2);
+
+    // Подписываем оси Ox и Oy
+    ui->graph_second_derivative->xAxis->setLabel("Ось X");
+    ui->graph_second_derivative->yAxis->setLabel("Ось Y");
+
+    // Установим область, которая будет показываться на графике
+    ui->graph_second_derivative->xAxis->setRange(x_min, x_max); // Для оси Ox
+    ui->graph_second_derivative->yAxis->setRange(y_min, y_max); // Для оси Oy
+
+    ui->graph_second_derivative->plotLayout()->insertRow(0); // Вставляем строку
+    ui->graph_second_derivative->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_second_derivative, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
+
+    ui->graph_second_derivative->replot();
 }
 
 #endif // CHARTS_H
--- widget.cpp
+++ widget.cpp
@@ -4,7 +4,8 @@
 #include "charts.h"
 #include <QDebug>
 
-void point_distance(const int& n, const int& p, const std::vector<double>& u_vector, const QVector<QVector<double>>& b,
+// Проецирует точку в пространстве на кривую
+void pointProjection(const int& n, const int& p, const std::vector<double>& u_vector, const QVector<QVector<double>>& b,
                     const std::vector<double>& h, const QPair<double, double>& point, Ui::Widget* ui)
 {
     QVector<Point_curve> point_u(1); // Точка кривой
@@ -38,143 +39,6 @@ void point_distance(const int& n, const int& p, const std::vector<double>& u_vec
     line->end->setCoords(point.first, point.second);
 
     ui->graph_function->replot();
-}
-
-
-
-
-void function_plot(const QVector<QVector<double>>& data_point, const QVector<Point_curve>& data_NURBS,
-                   const int& x_min, const int& x_max, const int& y_min, const int& y_max,
-                   const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
-{
-    ui->graph_function->clearGraphs(); // Очищаем все графики
-
-    QCPCurve *curve_point = new QCPCurve(ui->graph_function->xAxis, ui->graph_function->yAxis);
-
-    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
-    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
-    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
-
-    QPen pen;
-    pen.setWidth(2); // Устанавливаем ширину
-    curve_point->setPen(pen);
-
-    for(const auto& el: data_point) // Рисуем точки
-        curve_point->addData(el[0], el[1]);
-
-    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
-
-    QCPCurve *curve_spline = new QCPCurve(ui->graph_function->xAxis, ui->graph_function->yAxis);
-    pen.setColor(QColor(30, 144, 255));
-    curve_spline->setPen(pen);
-
-    for(const auto& el: data_NURBS) // Рисуем сплайн
-        curve_spline->addData(el.curve.first, el.curve.second);
-
-    ui->graph_function->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
-
-    ui->graph_function->legend->setVisible(true); // Включаем Легенду графика
-    curve_point->setName(labels_legend_1);
-    curve_spline->setName(labels_legend_2);
-
-    // Подписываем оси Ox и Oy
-    ui->graph_function->xAxis->setLabel("Ось X");
-    ui->graph_function->yAxis->setLabel("Ось Y");
-
-    // Установим область, которая будет показываться на графике
-    ui->graph_function->xAxis->setRange(x_min, x_max); // Для оси Ox
-    ui->graph_function->yAxis->setRange(y_min, y_max); // Для оси Oy
-
-    ui->graph_function->plotLayout()->insertRow(0); // Вставляем строку
-    ui->graph_function->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_function, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
-
-    ui->graph_function->replot();
-}
-
-void first_derivative_plot(const QVector<QVector<double>>& data_point, const QVector<Point_curve>& data_NURBS,
-                           const int& x_min, const int& x_max, const int& y_min, const int& y_max,
-                           const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
-{
-    ui->graph_first_derivative->clearGraphs(); // Очищаем все графики
-
-    QCPCurve *curve_point = new QCPCurve(ui->graph_first_derivative->xAxis, ui->graph_first_derivative->yAxis);
-
-    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
-    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
-    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
-
-    for(const auto& el: data_point) // Рисуем точки
-        curve_point->addData(el[0], el[1]);
-
-    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
-
-    QCPCurve *curve_deriv { nullptr };
-    curve_deriv = new QCPCurve(ui->graph_first_derivative->xAxis, ui->graph_first_derivative->yAxis);
-
-    for(const auto& el: data_NURBS) // Рисуем сплайн
-        curve_deriv->addData(el.derivative_1.first, el.derivative_1.second);
-
-    ui->graph_first_derivative->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
-
-    ui->graph_first_derivative->legend->setVisible(true); // Включаем Легенду графика
-    curve_point->setName(labels_legend_1);
-    curve_deriv->setName(labels_legend_2);
-
-    // Подписываем оси Ox и Oy
-    ui->graph_first_derivative->xAxis->setLabel("Ось X");
-    ui->graph_first_derivative->yAxis->setLabel("Ось Y");
-
-    // Установим область, которая будет показываться на графике
-    ui->graph_first_derivative->xAxis->setRange(x_min, x_max); // Для оси Ox
-    ui->graph_first_derivative->yAxis->setRange(y_min, y_max); // Для оси Oy
-
-    ui->graph_first_derivative->plotLayout()->insertRow(0); // Вставляем строку
-    ui->graph_first_derivative->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_first_derivative, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
-
-    ui->graph_first_derivative->replot();
-}
-
-void second_derivative_plot(const QVector<QVector<double>>& data_point, const QVector<Point_curve>& data_NURBS,
-                           const int& x_min, const int& x_max, const int& y_min, const int& y_max,
-                           const QString& title, const QString& labels_legend_1, const QString& labels_legend_2, Ui::Widget* ui)
-{
-    ui->graph_second_derivative->clearGraphs(); // Очищаем все графики
-
-    QCPCurve *curve_point = new QCPCurve(ui->graph_second_derivative->xAxis, ui->graph_second_derivative->yAxis);
-
-    curve_point->setPen(QColor(0, 0, 0, 255)); // Задаем чёрный цвет
-    curve_point->setLineStyle(QCPCurve::lsNone); // Убираем линии
-    curve_point->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 6)); // Формируем вид точек
-
-    for(const auto& el: data_point) // Рисуем точки
-        curve_point->addData(el[0], el[1]);
-
-    curve_point->setLineStyle(QCPCurve::lsLine); // Добавляем линии
-
-    QCPCurve *curve_deriv { nullptr };
-    curve_deriv = new QCPCurve(ui->graph_second_derivative->xAxis, ui->graph_second_derivative->yAxis);
-
-    for(const auto& el: data_NURBS) // Рисуем сплайн
-        curve_deriv->addData(el.derivative_2.first, el.derivative_2.second);
-
-    ui->graph_second_derivative->setInteractions(QCP :: iRangeDrag | QCP :: iRangeZoom); // Перетаскиваемый + масштабирование колеса прокрутки
-
-    ui->graph_second_derivative->legend->setVisible(true); // Включаем Легенду графика
-    curve_point->setName(labels_legend_1);
-    curve_deriv->setName(labels_legend_2);
-
-    // Подписываем оси Ox и Oy
-    ui->graph_second_derivative->xAxis->setLabel("Ось X");
-    ui->graph_second_derivative->yAxis->setLabel("Ось Y");
-
-    // Установим область, которая будет показываться на графике
-    ui->graph_second_derivative->xAxis->setRange(x_min, x_max); // Для оси Ox
-    ui->graph_second_derivative->yAxis->setRange(y_min, y_max); // Для оси Oy
-
-    ui->graph_second_derivative->plotLayout()->insertRow(0); // Вставляем строку
-    ui->graph_second_derivative->plotLayout()->addElement (0, 0, new QCPTextElement(ui->graph_second_derivative, title, QFont("sans", 12))); // Добавляем в первую строку и первый столбец заглавие
-
-    ui->graph_second_derivative->replot();
 }
 
 Widget::Widget(QWidget *parent)
@@ -229,7 +93,7 @@ Widget::Widget(QWidget *parent)
     int x_min = 0, x_max = 20;
     int y_min = -5, y_max = 15;
 
-    function_plot(b, data_NURBS, x_min, x_max, y_min, y_max, title, labels_legend_1, labels_legend_2, ui);
+    curve_plot(b, data_NURBS, x_min, x_max, y_min, y_max, title, labels_legend_1, labels_legend_2, ui);
 
     title = "1-я прoизвдная B-сплайна 4-го порядка";
     x_min = -10, x_max = 30;
@@ -256,13 +120,12 @@ Widget::Widget(QWidget *parent)
         derivs_curve[i].derivative_2 = c2[2];
     }
 
-    plot_deriv_for_point(derivs_curve, ui);
+    derivative_point_line(derivs_curve, ui); // Рисует линию производной в точке (касательную)
 
     QPair<double, double> point(1, 4);
 
-    point_distance(n, p, u, b, h, point, ui);
+    pointProjection(n, p, u, b, h, point, ui);
 }
-
 
 Widget::~Widget()
 {
